3ae6dc373c714b3446ba4edb96ff0b8de8ab261b
pengpeng <pengpeng@haizitong.com>
2016-05-31 11:07:23 +0800

    test

---
+++ test-git/test-git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test-git
+//
+//  Created by haizitong6 on 16/5/31.
+//  Copyright © 2016年 haizitong6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test-git/test-git/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test-git
+//
+//  Created by haizitong6 on 16/5/31.
+//  Copyright © 2016年 haizitong6. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
